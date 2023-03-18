1a9390777c92a96c35e9e6a16ae823a74396065d
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-03-18 11:38:40 +0530

    Update virtual_assisstant.py
    added whatsapp automation 
    removed mail using appopener
    removed brave using appopener
    removed whatsapp using appopener
    removed vlc using appopener

--- virtual_assisstant.py
+++ virtual_assisstant.py
@@ -55,22 +55,19 @@ if __name__ == '__main__':
         print(text)
         if text == 0:
             continue
+            
         elif 'chrome' in text or 'google' in text:
             if 'open' in text:
                 AppOpener.open("google chrome", match_closest=True)
             elif 'close' in text:
                 AppOpener.close("google chrome", match_closest=True)
             elif 'search' in text:
-                s1=text.split()
-                s1.remove("search")
-                s1.remove("in")
-                s1.remove("chrome")
-                s2=" ".join(s1)
+                search=inputCommand()
                 driver = webdriver.Chrome("C:\\Users\\ranke\\Downloads\\chromedriver_win32\\chromedriver.exe")
                 driver.maximize_window()
                 driver.get("http://www.google.com")
                 search_bar = driver.find_element(By.NAME, "q")
-                search_bar.send_keys(s2)
+                search_bar.send_keys(search)
                 search_bar.submit()
                 time.sleep(3)
                 first_result = driver.find_element(By.CSS_SELECTOR, "h3")
@@ -78,27 +75,38 @@ if __name__ == '__main__':
                 time.sleep(3)
             elif 'close search' in text:
                 driver.close()
+                
         elif 'youtube' in text:
             if 'play' in text:
                 output("which video or movie should i play for you sir?")
                 video=inputCommand()
                 pywhatkit.playonyt(video)
-
+                
+        elif 'send' in text and 'whatsapp' in text:
+            output("For whom should i send the message sir?")
+            name=inputCommand()
+            with open('all contacts .vcf') as f:
+                vcards = vobject.readComponents(f.read())
+            l=[]
+            for vcard in vcards:
+                if hasattr(vcard, 'fn') and vcard.fn.value.lower() == name.lower():
+                    if hasattr(vcard, 'tel') and vcard.tel:
+                        for p_no in vcard.contents['tel']:
+                            ph_no = str(p_no.value)
+                            l.append(ph_no)
+            phone_number=l[0]
+            if len(phone_number)<13:
+                phone_number="+91"+phone_number
+            output("What message should i send to {}".format(name))
+            message=inputCommand()
+            pywhatkit.sendwhatmsg_instantly(phone_number,message)
+            
         elif 'vs code' in text or 'visual studio code' in text:
             if 'open' in text:
                 AppOpener.open("visual studio code", match_closest=True)
             elif 'close' in text:
                 AppOpener.close("visual studio code", match_closest=True)
-        elif 'mail' in text:
-            if 'open' in text:
-                AppOpener.open('mail')
-            elif 'close' in text:
-                AppOpener.close('mail')
-        elif 'vlc' in text:
-            if 'open' in text:
-                AppOpener.open('vlc media player')
-            elif 'close' in text:
-                AppOpener.close('vlc media player')
+                
         elif 'camera' in text:
             if 'open' in text:
                 AppOpener.open("camera", match_closest=True)
@@ -112,6 +120,7 @@ if __name__ == '__main__':
                 cv2.waitKey(5000)
                 cam.release()
                 cv2.destroyWindow("photo")
+                
         elif 'edge' in text:
             if 'open' in text:
                 AppOpener.open("msedge", match_closest=True)
@@ -119,18 +128,14 @@ if __name__ == '__main__':
             elif 'close' in text:
                 AppOpener.close("msedge", match_closest=True)
             elif 'search' in text:
-                s1 = text.split()
-                s1.remove("search")
-                s1.remove("in")
-                s1.remove("edge")
-                s2=" ".join(s1)
+                search=inputCommand()
                 edge_options = EdgeOptions()
                 edge_options.use_chromium = True
                 edge_options.add_argument("start-maximized")
                 driver = Edge(executable_path="C:\\Users\\ranke\\Harsha\\webdrivers\\edgedriver_win64\\msedgedriver.exe")
                 driver.get("https://www.bing.com")
                 search_bar = driver.find_element(By.NAME, "q")
-                search_bar.send_keys(s2)
+                search_bar.send_keys(search)
                 search_bar.submit()
                 time.sleep(3)
                 first_result = driver.find_element(By.CSS_SELECTOR,"h2")
@@ -138,28 +143,21 @@ if __name__ == '__main__':
                 time.sleep(10)
             elif 'close search' in text:
                 driver.close()
+                
         elif 'telegram' in text:
             if 'open' in text:
                 AppOpener.open("Telegram", match_closest=True)
             elif 'close' in text:
                 AppOpener.close("Telegram", match_closest=True)
-        elif 'whatsapp' in text:
-            if 'open' in text:
-                AppOpener.open("whatsapp", match_closest=True)
-            elif 'close' in text: 
-                AppOpener.close("whatsapp", match_closest=True)
-        
-        elif 'brave' in text:
-            if 'open' in text:
-                AppOpener.open('brave')
-            elif 'close' in text:
-                AppOpener.close('brave')   
+         
         elif "stop" in text or "exit" in text or "bye" in text:
             output("Ok Bye Sir")
             break
+            
         elif 'time' in text:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            output(f"the time is {strTime}")   
+            output(f"the time is {strTime}")  
+            
         else:
             output("Could you please repeat again")
         
