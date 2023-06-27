cf66fe311c09a35071da40ea533d8513048716d3
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-06-27 21:26:33 +0530

    Update virtual_assisstant.py

--- virtual_assisstant.py
+++ virtual_assisstant.py
@@ -1,18 +1,11 @@
 import datetime
-import pyttsx3
-import speech_recognition as sr
-import os
-import vobject
-import pywhatkit
 import webbrowser as wb
-import AppOpener
 import cv2
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from msedge.selenium_tools import Edge, EdgeOptions
-from selenium.webdriver.common.by import By
+import pyttsx3
 import pywhatkit
+import speech_recognition as sr
+import vobject
+from googlesearch import search
 
 user = "harsha"
 assistant = 'Jarvis'
@@ -21,17 +14,18 @@ assistant.setProperty('rate', 150)
 voices = assistant.getProperty("voices")
 assistant.setProperty("voice", voices[0].id)
 
+
 def output(audio):
     assistant.say(audio)
     assistant.runAndWait()
+
 
 def inputCommand():
     rec = sr.Recognizer()
     query = ""
     with sr.Microphone() as source:
         rec.pause_threshold = 1
-        
-        # rec.adjust_for_ambient_noise(source)
+        rec.adjust_for_ambient_noise(source)
         audio = rec.listen(source)
         try:
             query = rec.recognize_google(audio, language="en-IN")
@@ -39,19 +33,21 @@ def inputCommand():
             output("Sorry,I can't hear you!")
     return query
 
+
 def inputCommand1():
     rec = sr.Recognizer()
-    query = ""
+    query1 = ""
     with sr.Microphone() as source:
-        rec.pause_threshold = 5
+        rec.pause_threshold = 1
         output("How can i help you Sir?")
-        # rec.adjust_for_ambient_noise(source)
+        rec.adjust_for_ambient_noise(source)
         audio = rec.listen(source)
         try:
-            query = rec.recognize_google(audio, language="en-IN")
+            query1 = rec.recognize_google(audio, language="en-IN")
         except Exception as e:
             output("Sorry,I can't hear you!")
-    return query
+    return query1
+
 
 # chrome searching
 def chrome_search():
@@ -65,11 +61,13 @@ def chrome_search():
     if first_result:
         wb.open(first_result)
 
+
 # playing YouTube video
 def play_video_in_youtube():
     output("which video or movie should i play for you sir?")
     video = inputCommand()
     pywhatkit.playonyt(video)
+
 
 # capturing photo
 def capture_photo():
@@ -81,6 +79,8 @@ def capture_photo():
     cam.release()
     cv2.destroyWindow("photo")
 
+
+# sending whatsapp message
 def send_whatsapp_message():
     output("For whom should i send the message sir?")
     name = inputCommand()
@@ -100,6 +100,7 @@ def send_whatsapp_message():
     message = inputCommand()
     pywhatkit.sendwhatmsg_instantly(phone_number, message)
 
+
 # sending_email
 def send_mail():
     output("To whom should i send an email Sir?")
@@ -109,38 +110,36 @@ def send_mail():
     msg = inputCommand()
     print(msg)
     pywhatkit.send_mail("rankelassh@gmail.com", "nwpmbjhunqhtjgdb", "Testing", msg, target_email)
-        
+
 
 if __name__ == '__main__':
     output("Hello Sir! I am your personal desktop assistant")
     while 1:
         r = sr.Recognizer()
-        text = inputCommand1().lower()
-        print(text)
-        if text == 0:
-            continue            
-        elif 'chrome' in text or 'google' in text:
-            if 'search' in text:
+        query = inputCommand1().lower()
+        print(query)
+        if query == 0:
+            continue
+        elif 'chrome' in query or 'google' in query:
+            if 'search' in query:
                 chrome_search()
-        elif 'send' in text and 'whatsapp' in text:
+        elif 'send' in query and 'whatsapp' in query:
             send_whatsapp_message()
-        elif 'mail' in text:
-            if 'send' in text:
-                send_email()
-        elif 'youtube' in text:
-            if 'play' in text:
+        elif 'mail' in query:
+            if 'send' in query:
+                send_mail()
+        elif 'youtube' in query:
+            if 'play' in query:
                 play_video_in_youtube()
-        elif 'camera' in text:
-            if 'capture photo' in text:
-                capture_photo()     
-        elif "stop" in text or "exit" in text or "bye" in text:
+        elif 'camera' in query:
+            if 'capture a photo' in query:
+                capture_photo()
+        # quiting virtual assistant
+        elif "stop" in query or "exit" in query or "bye" in query:
             output("Ok Bye Sir")
             break
-        
-        elif 'time' in text:
+        elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            output(f"the time is {strTime}")      
+            output(f"the time is {strTime}")
         else:
             output("Could you please repeat again")
-        
-    
