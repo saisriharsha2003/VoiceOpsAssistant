45e140127376c779dc82c8a9c6dc70a99a965b92
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-07-08 23:36:15 +0530

    added live_location.py

---
+++ live_location.py
@@ -0,0 +1,11 @@
+
+# from geopy.geocoders import ArcGIS
+# geolocator = ArcGIS(scheme="https")
+
+# loc_home=geolocator.geocode("Acharya Nagarjuna University")
+# print(loc_home.latitude,loc_home.longitude)
+import geo
+
+print(geo.getIP())
+
+
