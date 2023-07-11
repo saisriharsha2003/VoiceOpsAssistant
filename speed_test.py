9ced12d01a32d70aff4a6bff653086c7295ae5da
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-07-11 08:54:33 +0530

    modified

--- speed_test.py
+++ speed_test.py
@@ -1,11 +1,13 @@
 import speedtest
 
+
 def check_network_speed():
     st = speedtest.Speedtest()
-    download_speed = st.download() / 10**6  # Convert to Mbps
-    upload_speed = st.upload() / 10**6  # Convert to Mbps
+    download_speed = st.download() / 10 ** 6  # Convert to Mbps
+    upload_speed = st.upload() / 10 ** 6  # Convert to Mbps
 
     print(f"Download Speed: {download_speed:.2f} Mbps")
     print(f"Upload Speed: {upload_speed:.2f} Mbps")
+
 
 check_network_speed()
