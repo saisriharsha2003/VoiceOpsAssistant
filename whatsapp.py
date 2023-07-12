0afb184a1719e6f141616e3ecd1c7113039cbf50
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-07-12 08:23:33 +0530

    updated code

--- whatsapp.py
+++ whatsapp.py
@@ -1,7 +1,18 @@
-from selenium import webdriver
-import AppOpener
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-driver=webdriver.Chrome()
-driver.get("https://web.whatsapp.com")
+import pywhatkit
+import vobject
+
+name = input()
+with open('all contacts .vcf') as f:
+    vcards = vobject.readComponents(f.read())
+l = []
+for vcard in vcards:
+    if hasattr(vcard, 'fn') and vcard.fn.value.lower() == name.lower():
+        if hasattr(vcard, 'tel') and vcard.tel:
+            for p_no in vcard.contents['tel']:
+                ph_no = str(p_no.value)
+                l.append(ph_no)
+phone_number = l[0]
+if len(phone_number) < 13:
+    phone_number = "+91" + phone_number
+message = input()
+pywhatkit.sendwhatmsg_instantly(phone_number, message)
