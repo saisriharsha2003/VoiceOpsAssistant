935bca0eaa966e74738191c7354db042e3023e26
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-07-22 10:02:38 +0530

    updated

--- speed_test.py
+++ speed_test.py
@@ -1,13 +1,2 @@
-import speedtest
-
-
-def check_network_speed():
-    st = speedtest.Speedtest()
-    download_speed = st.download() / 10 ** 6  # Convert to Mbps
-    upload_speed = st.upload() / 10 ** 6  # Convert to Mbps
-
-    print(f"Download Speed: {download_speed:.2f} Mbps")
-    print(f"Upload Speed: {upload_speed:.2f} Mbps")
-
-
-check_network_speed()
+import nltk
+nltk.download('punkt')
