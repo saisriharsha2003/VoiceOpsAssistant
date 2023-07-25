6d6dc0f16a2dec45db3d35d0a01dafbe84905392
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2023-07-25 12:43:08 +0530

    modified

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -4,6 +4,8 @@ import speedtest
 from config import owmapikey
 import pyautogui
 import pyttsx3
+from fuzzywuzzy import fuzz
+import yaml
 import pywhatkit
 import requests
 import speech_recognition as sr
@@ -24,6 +26,24 @@ assistant.setProperty("voice", voices[0].id)
 def output(audio):
     assistant.say(audio)
     assistant.runAndWait()
+
+
+def calculate_similarity(input_text, examples):
+    return max(fuzz.ratio(input_text.lower(), example.lower()) for example in examples)
+
+
+def finding_intents(user_input):
+    with open("C:\\Users\\ranke\\Harsha\\Projects\\Desktop Assistant\\intents.yaml", "r") as yaml_file:
+        intents_data = yaml.safe_load(yaml_file)
+    best_intent = None
+    max_similarity = 0
+    for intent_data in intents_data["intents"]:
+        intent_examples = intent_data["examples"].strip().split("\n")
+        similarity = calculate_similarity(user_input, intent_examples)
+        if similarity > max_similarity:
+            max_similarity = similarity
+            best_intent = intent_data["intent"]
+    return best_intent
 
 
 def inputCommand():
@@ -62,11 +82,12 @@ def inputCommand1():
     return query1
 
 
+# TODO: searching chrome not working properly
 # TODO: alarm
 # TODO: remember
 # TODO: GUI
 # TODO: download youtube video and play that video
-# TODO: random possword generator
+# TODO: random password generator
 # TODO: remember function
 # TODO: make a call
 # TODO: youtube controls.3
@@ -109,7 +130,7 @@ def chrome_search():
     res = "searching" + chrome_query + " for you sir...."
     print(desktop_assistant + ":" + res)
     output(res)
-    search_results = search(query, num_results=1)
+    search_results = search(chrome_query, num_results=1)
     first_result = next(search_results, None)
     if first_result:
         wb.open(first_result)
@@ -152,7 +173,7 @@ def get_temperature_of_city():
         output("Response:" + weather_data)
 
 
-# meaning og the word
+# meaning of the word
 def meaning_of_the_word(word):
     meaning_of_word = PyDictionary().meaning(word)
     pos_tag = list(meaning_of_word.keys())
@@ -260,7 +281,7 @@ def perform_translation():
     text = inputCommand()
     print(user + ":" + text)
     print(
-        desktop_assistant + ":Sure! wait a while,let me translate the given text from " + source + " language to "+target+" language")
+        desktop_assistant + ":Sure! wait a while,let me translate the given text from " + source + " language to " + target + " language")
     output("Sure! wait a while,let me translate the given text from " + source + " language to " + target + " language")
     translator = Translator(from_lang=source, to_lang=target)
     translation = translator.translate(text)
@@ -290,47 +311,45 @@ if __name__ == '__main__':
     wish_and_time()
     while 1:
         r = sr.Recognizer()
-        query = inputCommand1().lower()
-        print(user+":"+query)
-        if query == 0:
+        s_query = inputCommand1().lower()
+        intent = finding_intents(s_query)
+        print(user + ":" + s_query)
+        print(intent)
+        if s_query == 0:
             continue
-        elif 'chrome' in query or 'google' in query:
-            if 'search' in query:
-                chrome_search()
-        elif 'send' in query and 'whatsapp' in query:
+        elif intent == 'search_chrome':
+            chrome_search()
+        elif intent == 'send_whatsapp_message':
             send_whatsapp_message()
-        elif 'mail' in query:
-            if 'send' in query:
-                send_mail()
-        elif 'youtube' in query:
-            if 'play' in query:
-                play_video_in_youtube()
-        elif 'meaning' in query:
+        elif intent == 'send_email':
+            send_mail()
+        elif intent == 'play_youtube':
+            play_video_in_youtube()
+        elif intent == 'find_meaning':
             print(desktop_assistant + ":Which word do I need to explain its meaning for?!")
             output("Which word do I need to explain its meaning for?!")
-            word = query.split()
+            word = s_query.split()
             meaning_of_the_word(word[-1])
-        elif 'translate' in query:
+        elif intent == 'translate':
             perform_translation()
-        elif 'network' in query and 'speed' in query:
+        elif intent == 'check_network_speed':
             check_network_speed()
-        elif 'camera' in query:
-            if 'capture a photo' in query:
-                capture_photo()
-        elif 'screenshot' in query:
+        elif intent == 'capture_photo':
+            capture_photo()
+        elif intent == 'take_screenshot':
             screenshot()
-        elif "date" in query:
+        elif intent == 'get_date':
             c_date = str(datetime.date.today())
             output("Today's Date is " + c_date)
-        elif 'search' in query and 'file' in query:
+        elif intent == 'search_file':
             find_file_path()
-        elif "stop" in query or "exit" in query or "bye" in query:
-            output("Ok Bye Sir")
-            break
-        elif 'temperature' in query:
+        elif intent == 'get_temperature':
             get_temperature_of_city()
-        elif 'time' in query:
+        elif intent == 'get_time':
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             output(f"Now the time is {strTime}")
+        elif intent == 'stop_assistant':
+            output("Ok Bye Sir")
+            break
         else:
             output("Could you please repeat again")
