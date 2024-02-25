8f64932ccda5e3dbd1a9312d7318a97b91c9cea4
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2024-02-26 01:31:00 +0530

    added word meaning function in seperate file

---
+++ WordMeaning.py
@@ -0,0 +1,15 @@
+from nltk.corpus import wordnet
+from virtual_assistant import desktop_assistant, speak, user, inputCommand
+
+def meaning_of_the_word():
+    print(desktop_assistant + ":Which word do I need to explain its meaning for?!")
+    speak("Which word do I need to explain its meaning for?!")
+    word = inputCommand()
+    print(f"{user}: {word}")
+    synsets = wordnet.synsets(word)
+    if synsets:
+        meaning_of_word = synsets[0].definition()
+        print(meaning_of_word)
+        speak(f"The meaning of {word} is {meaning_of_word}")
+    else:
+        meaning_of_word = "Meaning not found."
