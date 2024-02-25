1f07b89ee0ac283eb1dd57b05184d85fc05c4388
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2024-02-26 01:31:15 +0530

    added playing youtube video function in seperate file

---
+++ YoutubeVideo.py
@@ -0,0 +1,11 @@
+import pywhatkit
+from virtual_assistant import inputCommand, speak, desktop_assistant, user
+
+def play_video_in_youtube():
+    print(desktop_assistant + ": which video or movie should i play for you sir?")
+    speak("which video or movie should i play for you sir?")
+    video_name = inputCommand()
+    print(user + ":" + video_name)
+    print(desktop_assistant + ": Playing " + video_name + " in youtube!")
+    speak("Playing " + video_name + " in youtube!")
+    pywhatkit.playonyt(video_name)
